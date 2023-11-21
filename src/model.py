0dbf127ec13877fca5754d4cc5e3fe2bdaa96612
Trutnev Aleksei <alexgiving@mail.ru>
2023-11-22 00:32:56 +0300

    freeze backbone

--- src/model.py
+++ src/model.py
@@ -4,10 +4,18 @@ from torchvision.models.detection.faster_rcnn import (
     FastRCNNPredictor)
 
 
+def dfs_freeze(model):
+    for _, child in model.named_children():
+        for param in child.parameters():
+            param.requires_grad = False
+        dfs_freeze(child)
+
+
 def get_model(num_classes: int) -> FasterRCNN:
     model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(
         weights=FasterRCNN_MobileNet_V3_Large_320_FPN_Weights.DEFAULT
         )
+    dfs_freeze(model)
 
     in_featured = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_featured, num_classes)
