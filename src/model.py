374fc65972593bdb0f22f54ad226cec761650a53
Trutnev Aleksei <alexgiving@mail.ru>
2023-11-22 01:06:07 +0300

    [MERGE] train pipe + speedup + eval pipe + refactoring
    [MERGE] train pipe + speedup + eval pipe + refactoring

---
+++ src/model.py
@@ -0,0 +1,22 @@
+import torchvision
+from torchvision.models.detection.faster_rcnn import (
+    FasterRCNN, FasterRCNN_MobileNet_V3_Large_320_FPN_Weights,
+    FastRCNNPredictor)
+
+
+def dfs_freeze(model):
+    for _, child in model.named_children():
+        for param in child.parameters():
+            param.requires_grad = False
+        dfs_freeze(child)
+
+
+def get_model(num_classes: int) -> FasterRCNN:
+    model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(
+        weights=FasterRCNN_MobileNet_V3_Large_320_FPN_Weights.DEFAULT
+        )
+    dfs_freeze(model)
+
+    in_featured = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_featured, num_classes)
+    return model
