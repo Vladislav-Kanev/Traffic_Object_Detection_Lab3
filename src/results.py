50870a6998c23fc040a6b3d3cb9fe992f32e3d86
Trutnev Aleksei <alexgiving@mail.ru>
2023-11-23 01:15:21 +0300

    beautification

--- src/results.py
+++ src/results.py
@@ -7,7 +7,7 @@ from src.trainer import SubmissionType
 def prepare_result(results: SubmissionType, result_path: str, index_map : list[int]) -> None:
     submission_result = []
     global_id = 0
-    for result_id, sample_result in enumerate(tqdm(results)):
+    for result_id, sample_result in enumerate(tqdm(results), desc=f'Processing'):
         image_id = index_map[result_id]
         
         for bbox, category_id, score in zip(sample_result['boxes'], sample_result['labels'],
@@ -23,5 +23,7 @@ def prepare_result(results: SubmissionType, result_path: str, index_map : list[i
             )
             global_id += 1
     submission_results_pd = pd.DataFrame(submission_result)
+
     with open(result_path, mode='w') as file_descr:
         submission_results_pd.to_csv(file_descr)
+    print(f'Submission file is saved as {result_path}', flush=True)
